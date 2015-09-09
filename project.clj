545e5b8b53e697d2f3bc5888a13df1ef974608c3
Marcos <marcoslamuria@gmail.com>
2015-09-09 17:02:28 +0100

    bumpup version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/postgres-gateway "0.9.7"
+(defproject clanhr/postgres-gateway "0.9.8"
   :description "ClanHR postgres-gateway"
   :url "https://github.com/clanhr/postgres-gateway"
 
