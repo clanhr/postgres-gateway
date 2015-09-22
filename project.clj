6d1f2983e24484fdf94208135bc5b6a02dceaeef
Pedro Santos <donbonifacio@gmail.com>
2015-09-22 10:17:53 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -9,12 +9,13 @@
 
   :min-lein-version "2.5.0"
 
-  :dependencies [[environ "1.0.0"]
+  :dependencies [[environ "1.0.1"]
                  [org.clojure/clojure "1.7.0-RC1"]
+                 [org.clojure/java.jdbc "0.4.2"]
                  [alaisi/postgres.async "0.6.0"]
                  [cheshire "5.5.0"]
-                 [clanhr/result "0.6.0"]
-                 [clanhr/analytics "1.1.0"]]
+                 [clanhr/result "0.9.3"]
+                 [clanhr/analytics "1.4.0"]]
 
   :plugins [[lein-environ "1.0.0"]
             [lein-ancient "0.6.5"]]
