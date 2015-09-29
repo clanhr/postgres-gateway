8f301d81118a13a9cf8de869a1ce24f50ebb6439
Marcos <marcoslamuria@gmail.com>
2015-09-29 13:45:46 +0100

    change update to update-models

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/postgres-gateway "0.9.9"
+(defproject clanhr/postgres-gateway "1.0.0"
   :description "ClanHR postgres-gateway"
   :url "https://github.com/clanhr/postgres-gateway"
 
